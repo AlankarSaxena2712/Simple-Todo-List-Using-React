39cab1a6ee369b0816581196ee370e5ff9adcbc7
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-08-21 10:16:30 +0530

    Updates

---
+++ precache-manifest.a8fa551dfa470a9ef09cd060a2aba131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81d067f31970fb5387f6721e1ba78a3",
+    "url": "/Simple-Todo-List-Using-React/index.html"
+  },
+  {
+    "revision": "9b90078f6c8022e2a8d5",
+    "url": "/Simple-Todo-List-Using-React/static/css/main.7f687187.chunk.css"
+  },
+  {
+    "revision": "ccd61e9b333d9ac9dcd6",
+    "url": "/Simple-Todo-List-Using-React/static/js/2.986cdae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-Todo-List-Using-React/static/js/2.986cdae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b90078f6c8022e2a8d5",
+    "url": "/Simple-Todo-List-Using-React/static/js/main.2e2e52c8.chunk.js"
+  },
+  {
+    "revision": "cc02532f0665c3b253fe",
+    "url": "/Simple-Todo-List-Using-React/static/js/runtime-main.534ec99d.js"
+  }
+]);
